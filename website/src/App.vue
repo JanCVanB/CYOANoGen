e39b9d2414e11e30bfa0a021ef041267a6f2da7c
Jan Van Bruggen <jancvanbruggen@gmail.com>
2017-11-04 03:37:53 -0700

    Add Aventure component to render pre-generated adventures

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,19 +1,59 @@
 <template lang="pug">
   #app
+    h2 Choose Your Own Adventure novel generator
+    el-button.new-adventure(
+      v-bind:disabled='!!adventure'
+      v-on:click='adventure = onlyAdventureOption'
+    )
+      | {{ adventure ? 'Generate New Adventure' : 'Generate Adventure' }}
+    adventure(
+      v-if='adventure'
+      v-bind:adventure='adventure'
+    )
 </template>
 
 <script>
+import Adventure from './components/Adventure'
+
+const THE_SPOOKY_SCARY_SKELETON = {
+  title: 'The Spooky Scary Skeleton'
+}
+
 export default {
 
-  name: 'app'
+  name: 'app',
+
+  data () {
+    return {
+      adventure: null,
+      onlyAdventureOption: THE_SPOOKY_SCARY_SKELETON
+    }
+  },
+
+  components: {
+    Adventure
+  }
 
 }
 </script>
 
 <style lang="sass">
+html, body, #app
+  height: 100%
+  margin: 0
+  padding: 0
+  width: 100%
+
 #app
+  box-sizing: border-box
   font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
+  padding: 60px
   text-align: center
+</style>
+
+<style lang="sass" scoped>
+  .new-adventure
+    margin: 20px 0
 </style>
---
+++ website/src/components/Adventure.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #adventure
+    h3 {{ adventure.title }}
+</template>
+
+<script>
+export default {
+
+  name: 'adventure',
+
+  props: {
+
+    adventure: {
+      type: Object,
+      required: true
+    }
+
+  }
+
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
