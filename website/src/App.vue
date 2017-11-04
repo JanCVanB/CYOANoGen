a8239326755de9928079645845438635533d295e
Jan Van Bruggen <jancvanbruggen@gmail.com>
2017-11-04 02:48:08 -0700

    Remove HelloWorld component

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
+  name: 'app'
+
 }
 </script>
 
