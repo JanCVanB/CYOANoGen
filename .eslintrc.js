5dbe9995dedfca58e023f3a1995b266d46604401
Jan Van Bruggen <jancvanbruggen@gmail.com>
2017-11-24 12:21:38 -0800

    :wrench: Add a top-level ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // add your custom rules here
+  'rules': {
+    // warn about unused variables, rather than erroring
+    'no-unused-vars': 1
+  }
+}
